9dd474dad09ccdc53f9bfd1e59cb90a548635d3c
gusepepe <75131187+gusepepe@users.noreply.github.com>
2023-02-19 18:03:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,63 @@
 from src.Extractor import Extractor, PDFSentenceExtractionStrategy, OKRSentenceExtractionStrategy
 from src.Filter import filter_words
 from src.Exporter import generate_index
+from src.Slide import Slide
 from src.SentenceParser import SentenceParser
+import os, glob
+import nltk
 
-input_paths = [
-    "ressources\\test_data\\V1.pdf",
-    "ressources\\test_data\\V2.pdf",
-    "ressources\\test_data\\V3.pdf",
-    "ressources\\test_data\\V4.pdf",
-    "ressources\\test_data\\V5.pdf",
-    "ressources\\test_data\\V6.pdf",
-    "ressources\\test_data\\V7.1.pdf",
-    "ressources\\test_data\\V8.pdf",
-    "ressources\\test_data\\V9.pdf",
-    "ressources\\test_data\\V10.pdf",
-    "ressources\\test_data\\V11.pdf",
-    "ressources\\test_data\\V12.pdf",
-    "ressources\\test_data\\V13.pdf"
-]
-grouping = (3, 3)  # not active yet
-output_path = ""
 
-print("start extraction")
-extractor = Extractor(PDFSentenceExtractionStrategy())
-slides = extractor.extract_slides(input_paths)
+def main():
+    input_paths_backup = [
+        "ressources\\test_data\\V1.pdf",
+        "ressources\\test_data\\V2.pdf",
+        "ressources\\test_data\\V3.pdf",
+        "ressources\\test_data\\V4.pdf",
+        "ressources\\test_data\\V5.pdf",
+        "ressources\\test_data\\V6.pdf",
+        "ressources\\test_data\\V7.1.pdf",
+        "ressources\\test_data\\V8.pdf",
+        "ressources\\test_data\\V9.pdf",
+        "ressources\\test_data\\V10.pdf",
+        "ressources\\test_data\\V11.pdf",
+        "ressources\\test_data\\V12.pdf",
+        "ressources\\test_data\\V13.pdf"
+    ]
+    input_paths = get_pdf_files()
 
-print("start parsing")
-parser = SentenceParser(None)
-words = parser.parse_slides(slides)
+    grouping = (3, 3)  # not active yet
+    output_path = ""
 
-print("start filter")
-# filter = Filter(None)
-words = filter_words(words)  # needs to be rewritten as a Class
+    print("start extraction")
+    extractor = Extractor(PDFSentenceExtractionStrategy())
+    slides = extractor.extract_slides(input_paths)
 
-print("start export")
-index = generate_index(words, output_path, grouping)
-# document = exporter.gernerate_document(words, output_path)
+    print("start parsing")
+    parser = SentenceParser(None)
+    words = parser.parse_slides(slides)
+
+    print("start filter")
+    # filter = Filter(None)
+    words = filter_words(words)  # needs to be rewritten as a Class
+
+    print("start export")
+    index = generate_index(words, output_path, grouping)
+    # document = exporter.gernerate_document(words, output_path)
+
+
+def get_pdf_files():
+    dir_name = "ressources"
+    if not os.path.isdir(dir_name):
+        raise FileNotFoundError(f"{dir_name} directory not found")
+    pdf_files = glob.glob(os.path.join(dir_name, "**/*.pdf"), recursive=True)
+    return pdf_files
+
+def debug():
+    #TODO Für neue Benutzer geht der Code nicht, Sie müssen als erstes nltk.download benutzen. Da müssen wir noch eine Lösung finden.
+    nltk.download('stopwords')
+    pdf = get_pdf_files()
+    print(pdf)
+
+
+if __name__ == '__main__':
+    main()
